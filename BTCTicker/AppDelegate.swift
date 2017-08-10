31f47cbe6d76ee22a73cb2af145d0f0e937c6a24
Marc <marc.oneill2@gmail.com>
2017-08-10 10:42:48 +1000

    Initial Commit

---
+++ BTCTicker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BTCTicker
+//
+//  Created by Marc O'Neill on 10/08/2017.
+//  Copyright © 2017 marcexmachina. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ BTCTicker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BTCTicker
+//
+//  Created by Marc O'Neill on 10/08/2017.
+//  Copyright © 2017 marcexmachina. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
