07431542ed124672c921ec9fdb0a1c7c0aee3d29
Marc <marc.oneill2@gmail.com>
2017-08-10 10:52:22 +1000

    Initial commit

---
+++ BTCToday/ListRowViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ListRowViewController.swift
+//  BTCToday
+//
+//  Created by Marc O'Neill on 10/08/2017.
+//  Copyright © 2017 marcexmachina. All rights reserved.
+//
+
+import Cocoa
+
+class ListRowViewController: NSViewController {
+
+    override var nibName: String? {
+        return "ListRowViewController"
+    }
+
+    override func loadView() {
+        super.loadView()
+
+        // Insert code here to customize the view
+    }
+
+}
